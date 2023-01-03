9f77bbd921d04ff8519a97740f28d0f428ef2642
golibdev <gnarzullayev2000@gmail.com>
2023-01-04 01:15:15 +0500

    updated logo

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
    const theme = useTheme();
    return (
       <Typography fontWeight={700} fontSize="1.7rem">
-         New<span style={{
+         NewApp<span style={{
             color: theme.palette.primary.main
          }}>Flix</span>
       </Typography>
